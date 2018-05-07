2ecc23db49b28a51e07758b4968d8bcd54ad18d3
yangyzp <513993979@qq.com>
2018-05-07 11:39:52 +0800

    Add files via upload

---
+++ apiconfig.py
@@ -0,0 +1,47 @@
+﻿# Config
+NODE_ID = 1
+
+
+# hour,set 0 to disable
+SPEEDTEST = 0
+CLOUDSAFE = 1
+ANTISSATTACK = 0
+AUTOEXEC = 0
+
+MU_SUFFIX = 'v1m3cc2x'
+MU_REGEX = '%5m'
+
+SERVER_PUB_ADDR = '127.0.0.1'  # mujson_mgr need this to generate ssr link
+API_INTERFACE = 'glzjinmod'  # glzjinmod, modwebapi
+
+WEBAPI_URL = 'https://zhaoj.in'
+WEBAPI_TOKEN = 'glzjin'
+
+# mudb
+MUDB_FILE = 'mudb.json'
+
+# Mysql
+MYSQL_HOST = '23.234.197.24'
+MYSQL_PORT = 3306
+MYSQL_USER = 'sspanel'
+MYSQL_PASS = 'qq469566135'
+MYSQL_DB = 'sspanel'
+
+MYSQL_SSL_ENABLE = 0
+MYSQL_SSL_CA = ''
+MYSQL_SSL_CERT = ''
+MYSQL_SSL_KEY = ''
+
+# API
+API_HOST = '127.0.0.1'
+API_PORT = 80
+API_PATH = '/mu/v2/'
+API_TOKEN = 'abcdef'
+API_UPDATE_TIME = 60
+
+# Manager (ignore this)
+MANAGE_PASS = 'ss233333333'
+# if you want manage in other server you should set this value to global ip
+MANAGE_BIND_IP = '127.0.0.1'
+# make sure this port is idle
+MANAGE_PORT = 23333
